c9008163cb8d191f2ba62f81478c9546115d2937
devd <denisio.bro@gmail.com>
2021-07-24 05:09:01 +0300

    update

--- maize-blockchain-gui/src/util/computeStatistics.ts
+++ maize-blockchain-gui/src/util/computeStatistics.ts
@@ -4,7 +4,7 @@ import type Wallet from '../types/Wallet';
 
 // deprecated
 export default function computeStatistics(wallets: Wallet[]): {
-  totalChiaFarmed: Big;
+  totalMaizeFarmed: Big;
   biggestHeight: number;
   biggestRewardHeight: number;
   poolCoins: Big;
@@ -55,7 +55,7 @@ export default function computeStatistics(wallets: Wallet[]): {
     });
   });
 
-  const totalChiaFarmed = poolCoins.plus(farmerCoins);
+  const totalMaizeFarmed = poolCoins.plus(farmerCoins);
   const totalBlockRewards = Big(poolCoins).times(8).div(7);
   const userTransactionFees = Big(farmerCoins).minus(
     Big(totalBlockRewards).div(8),
@@ -65,7 +65,7 @@ export default function computeStatistics(wallets: Wallet[]): {
     .minus(userTransactionFees);
 
   return {
-    totalChiaFarmed,
+    totalMaizeFarmed,
     biggestHeight,
     biggestRewardHeight,
     poolCoins,
